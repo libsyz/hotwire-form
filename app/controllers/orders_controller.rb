e623c9bdf9cd3a1d5627d0eb8d3ef7f14fe62abf
miguel jimenez <denkatalabeno@gmail.com>
2021-03-23 21:59:10 +0800

    going back and forth working out

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,30 @@
 class OrdersController < ApplicationController
+
   def new
     @order = params[:order_id] ? Order.find(params[:order_id]) : Order.new
+    binding.pry
   end
 
   def billing
-    if params[:order][:order_id]
-      @order = Order.find(params[:order_id])
-      @order.update(order.params)
+    binding.pry
+    if permitted_params[:order_id]
+      @order = Order.find(permitted_params[:order_id])
+      @order.update(billing_details: permitted_params[:billing_details])
     else
-      @order = Order.new(order_params)
+      @order = Order.new(permitted_params)
       @order.save
     end
   end
 
   def shipping
-    @order = Order.find(params[:order][:order_id])
-    @order.shipping_address = order_params[:shipping_address]
+    @order = Order.find(permitted_params[:order_id])
+    @order.shipping_address = permitted_params[:shipping_address]
+    @order.save
   end
 
   private
 
-  def order_params
+  def permitted_params
     allowed = params.require(:order).permit(:billing_details, :status, :shipping_address, :order_id)
     allowed[:status] = allowed[:status].to_i
     allowed
